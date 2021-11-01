96bb023896d284c0fae577337a7873364461c0a1
stonetek <pelkorstone@gmail.com>
2021-11-01 15:04:35 -0300

    Projetos criados

---
+++ backend/src/test/java/com/devduperior/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devduperior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
