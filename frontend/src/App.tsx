50f7b6ecc3bd7114f105572f5f4b211bb971bddd
stonetek <pelkorstone@gmail.com>
2021-11-01 17:20:14 -0300

    Basic statics componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/navbar";
+
 function App() {
   return (
-    <div >
-      <h1 className="text-primary" >Avante Eorlingas!!!!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary" >Avante Eorlingas!!!!</h1>
+
+        <DataTable />
+
+      </div>
+      <Footer />
+    </>
   );
 }
 
