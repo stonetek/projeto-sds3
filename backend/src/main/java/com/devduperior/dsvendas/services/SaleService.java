7f74aadd507429af5f69a8c920aae652c9bacf97
stonetek <pelkorstone@gmail.com>
2021-11-04 23:02:06 -0300

    |Group by search

--- backend/src/main/java/com/devduperior/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devduperior/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.devduperior.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devduperior.dsvendas.dto.SaleDTO;
+import com.devduperior.dsvendas.dto.SaleSuccessDTO;
+import com.devduperior.dsvendas.dto.SaleSumDTO;
 import com.devduperior.dsvendas.entities.Sale;
 import com.devduperior.dsvendas.repositories.SaleRepository;
 import com.devduperior.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
